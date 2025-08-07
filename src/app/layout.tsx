42e5208ece88e75d062cb00b768319b548ca7a6a
ameerza <ameerza@outlook.com>
2025-08-07 22:23:02 +1000

    Changed font to better match

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Source_Sans_3 } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const sourceSans = Source_Sans_3({
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
   title: "BS Generator",
@@ -25,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={sourceSans.className}
       >
         {children}
       </body>
