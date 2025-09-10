6b278412bac9ba39dbb3736420d5510c0c127e02
ameerza <ameerza@outlook.com>
2025-09-11 00:41:33 +1000

    Added Vercel's Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Source_Sans_3 } from "next/font/google";
 import "./globals.css";
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const sourceSans = Source_Sans_3({
   subsets: ['latin'],
@@ -24,6 +25,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
